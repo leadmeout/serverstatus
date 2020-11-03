e316c3e7d71b8e8401a4efecffc8370e23ed259c
Mark Horvat <markhorvat@live.ca>
2020-11-03 18:04:49 +0100

    removed sensitive data from sendsms file

--- sendsms.py
+++ sendsms.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 # Access Twilio account using Account SID and Auth Token
-client = Client("AC48bc55917ac176889839e430d151e0e4", "ab5b4c8d5f0835fc9e7a9a8aa50bd2db")
+client = Client("", "")
 
 
 def sendsms():
