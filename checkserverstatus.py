395e90c44f61de23bb86eda44c7dcc72358dcc61
leadmeout <markhorvat@live.ca>
2020-11-08 14:22:03 +0100

    Updated file to reflect linux OS
    Removed subprocess module and added a sleep function to prevent cronjob from sending a text every minute

--- checkserverstatus.py
+++ checkserverstatus.py
@@ -1,4 +1,5 @@
-import subprocess
+import os
+import time
 
 from sendsms import sendsms
 
@@ -8,26 +9,15 @@ server = '8.8.8.8'
 def check_ping(server):
 
 	'''
-	Function to ping the server/host.
+	Function to ping the server/host and return the result.
 	
-	This uses the subprocess module, which allows you to spawn new processes, 
-	connect to their input/output/error pipes, and obtain their return codes.
 	'''
 
-	# Define a command and convert it into list format
-	command = ("ping -c 1 " + server).split()
+	# create a variable that 
 
-	# Run external program (in this case, ping) using subprocess module and its Popen() function
-	# FYI: The P stands for Process
-	# We pipe the output of subprocess.Popen() to stdout or stderr (depending on the outcome of the ping command)
-	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# And we use the communicate method to return both the stdout and stderr
-	stdout, stderr = process.communicate()
-	# The return code will either be 0 if there is a ping reply or 1 if there isn't
-	# We store the return code (either 0 or 1) in 
-	response = process.returncode
+	response = os.system("ping -c 1 " + server)
 
-	# Set pingstatus to return a boolean based on the result of the returncode
+	# Set pingstatus to return a boolean based on the value of response
 	if response == 0:
 		pingstatus = True
 	else:
@@ -38,20 +28,24 @@ def check_ping(server):
 
 def check_server_status(server):
 	'''
-		Function to check whether a server or host is available
-		If not, this function will call sendsms defined in sendsms.py to send a text message notification
+		Function to check whether a server or host is available.
+		If not, this function will call sendsms defined in sendsms.py to send a text message notification.
+		The process will pause for 30 minutes and send another notification if the host is still unreachable.
 	'''
 
 	server_status = check_ping(server)
 
 	if server_status != True:
 		sendsms()
+		while server_status == False: 
+			time.sleep(1800)
+			if server_status == True:
+				break
+			else:
+				sendsms()
+
 
 
 check_server_status(server)
-
-
-
-
 
 
